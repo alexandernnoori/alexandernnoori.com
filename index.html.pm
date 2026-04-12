c1ecdadff35380532bb2c6906c72320a1cb48775
Alexander Nabavi-Noori <alexander.nnoori@gmail.com>
2026-04-12 00:51:46 -0400

    Design changes to the home page

--- index.html.pm
+++ index.html.pm
@@ -5,10 +5,12 @@
 
 ◊small-topic{Alexander Nabavi-Noori}
 
-◊aside{Credit for this quote goes to ◊link["https://danhonig.info" "Dan Honig,"] who also features it on his website. It captures the human element of governance that has long motivated my work.}
-◊indented-justified{If we look at government agencies around us that stand out as ‘best,’ we will find they consist of cohesive groups of women and men who are ‘turned on’ by something. But by what? Not their paychecks, nor the latest reform gimmicks, but by the very work they are doing: stopping child abuse, fighting forest fires, battling epidemics.}
-◊sig{—Charles Goodsell, ◊book{Mission Mystique}}
+◊image-aside["images/Xander_BW.jpg"]
 
+◊indented-justified{If we look at government agencies around us that stand out as ‘best,’ we will find they consist of cohesive groups of women and men who are ‘turned on’ by something. But by what? Not their paychecks, nor the latest reform gimmicks, but by the very work they are doing: stopping child abuse, fighting forest fires, battling epidemics.}
+◊sig{—Charles Goodsell, ◊book{Mission Mystique*}}
+
+◊aside{*Credit for this quote goes to ◊link["https://danhonig.info" "Dan Honig,"] who also features it on his website. It captures the human element of governance that has long motivated my work.}
 I am a first-year Ph.D. student and Ashford Fellow in the Department of Government and Social Policy at ◊link["https://www.gov.harvard.edu/people/graduate-students/" "Harvard University."] I am also an affiliate of the ◊link["https://caps.gov.harvard.edu/directory/julian-perry/?cp-dir-id=2303" "Center for American Political Studies."] Before graduate school, I worked as a Special Assistant for immigration policy on the Senate Judiciary Committee, as a federal law clerk, and as an appellate staff attorney at the Department of Justice’s Civil Division. 
 
 My research centers on the bureaucacy, the administrative state, and Congress. For more information, see ◊xref["research.html" "Research"].
